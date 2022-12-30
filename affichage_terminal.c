24be5c9c971669db0b05d8126a52d68e8ff3dc11
Browkly <dsmmaxime50@gmail.com>
2022-12-30 15:04:26 +0100

    version 2.0

--- affichage_terminal.c
+++ affichage_terminal.c
@@ -13,8 +13,36 @@ void print_grid_terminal(grid *g) {
   for (i = 5; i >= 0; i--) {
     printf("|");
     for (j = 0; j < 7; j++) {
-      printf(" %d  |", (g->g)[i][j]);
+      switch ((g->g)[i][j]) {
+        case 0:
+          printf("   |");
+          break;
+        case 1:
+          printf(" x |");
+          break;
+        case 2:
+          printf(" o |");
+          break;
+        default:
+          break;
+      }
     }
     printf("\n");
   }
+}
+
+int round_human_terminal(void) {
+  int choice = -1;
+  int buffer_drainer;
+  do {
+    printf("Make your column choice (between 1 and 7):\n");
+    choice = getchar();
+    choice -=
+        48;  // Because getchar() gives us the ASCII code (ASCII code of 1 = 49)
+    do {
+      buffer_drainer = getchar();
+    } while (buffer_drainer != '\n' &&
+             buffer_drainer != EOF);  // We drain the buffer
+  } while (choice < 1 || choice > 7);
+  return choice;
 }
--- affichage_terminal.h
+++ affichage_terminal.h
@@ -2,7 +2,9 @@
 #define _AFFICHAGE_TERMINAL 0
 
 typedef struct _grid grid;
+typedef struct _game game;
 
 void print_grid_terminal(grid *g);
+int round_human_terminal(void);
 
 #endif
