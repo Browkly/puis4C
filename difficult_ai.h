6e1e92a0a0b46885377c37e66a3e09910f8f0a45
Benjamin <benjaminstawiarski@gmail.com>
2022-12-30 14:34:39 +0100

    commentaires et idées pour l'ia difficile

--- difficult_ai.h
+++ difficult_ai.h
@@ -2,4 +2,14 @@
 
 #define DIFFICULT_AI_H
 #include "medium_ai.h"
+
+// IA Difficile: Le script se chargera d'analyser à chaque nouveau coup, l'état
+// de la grille, et récupérera les coordonnées des plus grands alignements non
+// bloqués, du joueur enemi, et de l'ia.
+// Selon les alignements de l'enemi et de l'ia, le script choisira ou non
+// d'agrandir son alignement, de bloquer celui de l'enemi, ou de jouer au hasard
+
+// Prévision des alignements potentiels en fonction des cases vides à
+// implémenter (pour éviter par exemple la création d'un escalier de manière
+// involontaire etc ...)
 #endif
